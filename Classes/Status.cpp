c568b79266e717a3c84b7832b07825536560aa8f
PatricioSaldivar <patricio.saldivarf@gmail.com>
2019-12-02 20:52:05 -0600

    Made documentation of classes

--- Classes/Status.cpp
+++ Classes/Status.cpp
@@ -5,25 +5,26 @@
 #include <iostream>
 #include "Status.h"
 
-
+// Gets the status of the code
 int Status::getStatusCode() {
     return this->statusCode;
 }
-
+// Prints the result
 void Status::outputResult() {
     for (string &s : result)
         cout << s << endl;
 }
-
+//Constructor
 Status::Status() {
     this->statusCode = s_succes;
     this->result = vector<string>();
 }
-
+// Sets the status of the code
 void Status::setStatusCode(int statusCode) {
     this->statusCode = statusCode;
 }
 
+//Adds a string result
 void Status::addStringResult(string s) {
     this->result.push_back(s);
 }
